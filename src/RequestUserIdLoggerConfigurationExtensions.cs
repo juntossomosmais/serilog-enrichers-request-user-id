c4239e4f78f1800631313502347d6af196320f9f
Rafael Miranda <rafaelpadovezi@gmail.com>
2021-08-31 13:17:27 -0300

    feature: add code docstring

--- src/RequestUserIdLoggerConfigurationExtensions.cs
+++ src/RequestUserIdLoggerConfigurationExtensions.cs
@@ -4,8 +4,16 @@ using System;
 
 namespace Serilog
 {
+    /// <summary>
+    /// Extends <see cref="LoggerConfiguration"/> to add enrichers for request's UserId.
+    /// </summary>
     public static class RequestUserIdLoggerConfigurationExtensions
     {
+        /// <summary>
+        /// Enrich log events with a RequestUserId property containing the value of the user Id on ClaimsPrincipal.
+        /// </summary>
+        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
         public static LoggerConfiguration WithRequestUserId(this LoggerEnrichmentConfiguration enrichmentConfiguration)
         {
             if (enrichmentConfiguration is null)
--- tests/Support/DelegateSink.cs
+++ tests/Support/DelegateSink.cs
@@ -4,9 +4,13 @@ using System;
 
 namespace Serilog.Enrichers.RequestUserId.Tests.Support
 {
+    /// <summary>
+    /// Serilog Sink that calls the provided delegate with the log output.
+    /// Can be used to capture log events and use it on assertions.
+    /// </summary>
     public class DelegateSink : ILogEventSink
     {
-        readonly Action<LogEvent> _write;
+        private readonly Action<LogEvent> _write;
 
         public DelegateSink(Action<LogEvent> write)
         {
